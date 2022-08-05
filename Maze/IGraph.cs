05888c7824928f183b4513f4945188e345042fb8
Mårten Åsberg <marten.asberg@outlook.com>
2022-08-05 22:15:20 +0200

    Graph base

---
+++ Maze/IGraph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maze;
+
+public interface IGraph<INode>
+{
+	/// <summary>
+	/// Indicates if two are connected.
+	/// </summary>
+	/// <exception cref="ArgumentException">If the two nodes are not neighbours.</exception>
+	public bool this[INode a, INode b] { get; set; }
+
+	/// <summary>
+	/// Lists all neighbours of the <paramref name="current"/> node.
+	/// </summary>
+	public IEnumerable<INode> Neighbours(INode current);
+}
